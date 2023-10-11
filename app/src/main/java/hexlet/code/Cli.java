eb86ddfcfd0bd6961d978f7717824dc6ee5b7e98
Nikita Kravchenko <ni_kr_95@mail.ru>
2023-10-11 20:55:34 +0500

    add checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,7 +4,9 @@ public class Cli {
     public static void scan() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is your name?");
+        System.out.print("Please enter the game number and press Enter.\n1- Greet\n0- Exit  ");
+        String GameNumber = scanner.next();
+        System.out.print("Welcome to the Brain Games!\nWhat is your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
 
