5a3ade6a9fd21fcea7d7e62d953580c4a2aef44f
nikita <ni_kr_95@mail.ru>
2024-05-06 16:31:34 +0500

    Add Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
     mavenCentral()
 }
 
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
